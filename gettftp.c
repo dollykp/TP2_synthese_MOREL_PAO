8fb17e246bb828593a232a3a1079b948e61238e4
dollykp <dolly-kravanna.pao@ensea.fr>
2024-12-12 09:37:57 +0100

    Update gettftp.c

--- gettftp.c
+++ gettftp.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
-int main() {
-	
+	(void) argc;
+	printf("Name resolution for %s \n", argv[1]);
+	struct addrinfo hints;
+	struct addrinfo *res;
+	memset(&hints, 0, sizeof(hints));	
+	getaddrinfo(argv[1], NULL, &hints, &res);
 }

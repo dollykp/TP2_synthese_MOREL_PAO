eaae1f5e5ce97a49455612b44ae2f64a2750fc5e
dollykp <llysns53@gmail.com>
2024-12-12 11:03:14 +0100

    code to test the following terminal-command "./gettftp 12.0.0.1 1069 zeros256"

--- gettftp.c
+++ gettftp.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
-	(void) argc;
+	int address;
+	(void) argc; /*putting a void cast because the makefile doesn't allow unused variable*/
 	struct addrinfo hints;
 	struct addrinfo *res;
-	memset(&hints, 0, sizeof(hints));
-	hints	
-	getaddrinfo(argv[1], NULL, &hints, &res);
+	hints.ai_socktype = SOCK_DGRAM; /*An UDP server listens to a local port, and waits for a datagram.*/
+	hints.ai_family = AF_INET; /*IPv4*/
+	hints.ai_protocol = IPPROTO_UDP; /*UDP protocol*/
+	memset(&hints, 0, sizeof(hints));	
+	address = getaddrinfo(argv[1], argv[2], &hints, &res);
+	printf("The address is : %s \n", argv[1]);
+	printf("The port number is : %s \n", argv[2]);
+	printf("The file is : %s \n", argv[3]);
+	printf("The address is : %d \n", address);
 }

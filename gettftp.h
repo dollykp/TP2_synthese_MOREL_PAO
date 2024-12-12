aaa03df79b696d4c74854c518327f6d08fd5e8d4
dollykp <llysns53@gmail.com>
2024-12-12 10:56:30 +0100

    updating the header file

--- gettftp.h
+++ gettftp.h
@@ -1,10 +1,9 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
